399f965d145ce612547dc9df4cbfcc0849afb6fe
Anna <anna.aghajanyan987@gmail.com>
2022-05-21 07:17:33 -0700

    Add Farm Animals sound board project

---
+++ main.js
@@ -0,0 +1,26 @@
+const sounds = ["rooster", "hen", "cow", "horse", "sheep", "goat"];
+const buttonsContainer = document.querySelector("#buttons");
+
+const fragment = document.createDocumentFragment();
+
+sounds.forEach((sound) => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+    btn.style.backgroundImage = `url('images/${sound}.png')`;
+
+    fragment.appendChild(btn);
+    buttonsContainer.appendChild(fragment);
+
+    btn.addEventListener("click", (e) => {
+        stop();
+        document.querySelector(`#${sound}`).play();
+    });
+});
+
+function stop() {
+    sounds.forEach((sound) => {
+        const song = document.querySelector(`#${sound}`);
+        song.pause();
+        song.currentTime = 0;
+    });
+}
